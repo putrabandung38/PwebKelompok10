1a763416fd42ac6817ee46e5af3aece9de6fc665
Firman Alamsyah - 3IF-02 <41243463+frmnsp@users.noreply.github.com>
2019-07-19 18:53:35 +0700

    Update sequelize.js

--- configs/sequelize.js
+++ configs/sequelize.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('mysql://root:@localhost:3306/books_kelompok10');
-
+const sequelize = new Sequelize('books_kelompok10', 'root', 'password',{
+	host :'localhost',
+	dialect:'mysql'
+});
 module.exports = sequelize;
